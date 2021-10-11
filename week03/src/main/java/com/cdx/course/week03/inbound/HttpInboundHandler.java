25603dfb8bfacaf3435f3a309412f0743190eec3
陈东旭 <cdxuuu@126.com>
2021-10-11 14:45:27 +0800

    week03 netty msg handle

--- week03/src/main/java/com/cdx/course/week03/inbound/HttpInboundHandler.java
+++ week03/src/main/java/com/cdx/course/week03/inbound/HttpInboundHandler.java
@@ -1,15 +1,72 @@
 package com.cdx.course.week03.inbound;
 
+import com.cdx.course.week03.filter.HeaderHttpRequestFilter;
+import com.cdx.course.week03.filter.HttpRequestFilter;
+import com.cdx.course.week03.outbound.HttpOutboundHandler;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+import static org.apache.http.HttpHeaders.CONNECTION;
+
 @Slf4j
-public class HttpInboundHandler extends ChannelInboundHandlerAdapter{
+public class HttpInboundHandler extends ChannelInboundHandlerAdapter {
     private final List<String> proxyServer;
+
+    HttpOutboundHandler httpOutboundHandler;
+    HttpRequestFilter filter = new HeaderHttpRequestFilter();
 
     public HttpInboundHandler(List<String> proxyServer) {
         this.proxyServer = proxyServer;
     }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        FullHttpRequest request = (FullHttpRequest) msg;
+        String uri = request.uri();
+        httpOutboundHandler.handle(request, ctx, filter);
+    }
+
+
+    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        FullHttpResponse response = null;
+        try {
+            String value = "hello,cdx";
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+
+        } catch (Exception e) {
+            log.error("处理测试接口出错", e);
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
 }
