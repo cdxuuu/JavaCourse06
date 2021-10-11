aa2be46031999e696984e92d8501d317d3c5379c
陈东旭 <cdxuuu@126.com>
2021-10-11 14:57:18 +0800

    week03 netty msg handle

--- week03/src/main/java/com/cdx/course/week03/outbound/netty4/NettyHttpClientOutboundHandler.java
+++ week03/src/main/java/com/cdx/course/week03/outbound/netty4/NettyHttpClientOutboundHandler.java
@@ -2,21 +2,22 @@ package com.cdx.course.week03.outbound.netty4;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class NettyHttpClientOutboundHandler  extends ChannelInboundHandlerAdapter {
-    
+
     @Override
     public void channelActive(ChannelHandlerContext ctx)
             throws Exception {
-        
-        
+
+        log.info("active");
     }
-    
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg)
             throws Exception {
-        
-        
-        
+
+        log.info("client channel read:{}", msg);
     }
 }
