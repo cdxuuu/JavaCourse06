e09afc21665f482fcbdce50e61d4be3607353b13
陈东旭 <cdxuuu@126.com>
2021-10-24 18:52:36 +0800

    week04

---
+++ week05/src/main/java/com/cdx/course/week05/homeword/Student.java
@@ -0,0 +1,12 @@
+package com.cdx.course.week05.homeword;
+
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String name;
+
+    private Integer age;
+}
---
+++ week05/src/main/java/com/cdx/course/week05/homeword/StudentController.java
@@ -0,0 +1,10 @@
+package com.cdx.course.week05.homeword;
+
+
+import javax.annotation.Resource;
+public class StudentController {
+    @Resource(name = "student")
+    Student student;
+
+
+}
